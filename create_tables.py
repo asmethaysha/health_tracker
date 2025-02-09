05b9f8538be71011787c6b42ebf7cfb7b5d352ae
aaquibkhan <akaaquib@gmail.com>
2025-02-09 02:31:09 -0500

    GUI complete

--- create_tables.py
+++ create_tables.py
@@ -1,9 +1,26 @@
 import sqlite3
 
-conn = sqlite3.connect("healthfit.db")
-cursor = conn.cursor()
+def init_db():
+    conn = sqlite3.connect("healthfit.db")
+    cursor = conn.cursor()
 
-# Create SLEEP_TRACKING table without NULL constraints
+    cursor.execute("PRAGMA table_info(HISTORICAL_WEIGHT)")
+    table_info = cursor.fetchall()
+
+    if table_info:
+        print("Table HISTORICAL_WEIGHT exists.")
+    else:
+        print("Table HISTORICAL_WEIGHT does not exist.")
+    
+    conn.commit()
+    conn.close()
+
+
+"""
+    
+    
+
+    
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS SLEEP_TRACKING (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,22 +31,33 @@ cursor.execute('''
     )
 ''')
 
-# Initialize the database with EXERCISE_TRACKING table
 cursor.execute('''
-        CREATE TABLE IF NOT EXISTS EXERCISE_TRACKING (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            starting_timestamp TIMESTAMP,
-            ending_timestamp TIMESTAMP,
-            exercise_name VARCHAR,
-            calories_burned INTEGER,
-            repetitions INTEGER,
-            steps INTEGER,
-            comments VARCHAR
-        )
-    ''')
+    CREATE TABLE IF NOT EXISTS EXERCISE_TRACKING (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        starting_timestamp TIMESTAMP,
+        ending_timestamp TIMESTAMP,
+        exercise_name VARCHAR,
+        calories_burned INTEGER,
+        repetitions INTEGER,
+        steps INTEGER,
+        comments VARCHAR
+    )
+''')
+
+
+    conn.commit()
+
+    """
+
+    
+
+   
+
     
 
 
-conn.commit()
-conn.close()
-print("SLEEP_TRACKING table created successfully!")
+
+
+init_db()
+print("done")
+
--- server.py
+++ server.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 
 # Initialize the database with EXERCISE_TRACKING table
 def init_db():
-    conn = sqlite3.connect("workouts.db")
+    conn = sqlite3.connect("healthfit.db")
     cursor = conn.cursor()
+
+#exercise table
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS EXERCISE_TRACKING (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -20,6 +22,58 @@ def init_db():
             comments VARCHAR
         )
     ''')
+
+#weight and body fat
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS HISTORICAL_WEIGHT (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            weight FLOAT,
+            body_fat_percentage FLOAT
+        )
+    ''')
+
+
+# sleep table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS SLEEP_TRACKING (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            starting_timestamp TIMESTAMP,
+            ending_timestamp TIMESTAMP,
+            time_slept FLOAT,
+            comments VARCHAR
+        )
+    ''')
+
+# mood table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS MOOD_TRACKING (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            timestamp TIMESTAMP,
+            happiness INTEGER,
+            comments VARCHAR
+        )
+    ''')
+
+
+#meal table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS MEAL_TRACKING (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            meal_category VARCHAR,
+            calories FLOAT,
+            number_of_servings FLOAT,
+            weight_grams FLOAT,
+            vitamins FLOAT,
+            minerals FLOAT,
+            carbohydrates FLOAT,
+            protein FLOAT,
+            comments VARCHAR,
+            timestamp TIMESTAMP
+        )
+    ''')
+
+
     conn.commit()
     conn.close()
 
@@ -36,7 +90,7 @@ def log_workout():
         steps = request.form.get('steps')
         comments = request.form.get('comments')
 
-        conn = sqlite3.connect("workouts.db")
+        conn = sqlite3.connect("healthfit.db")
         cursor = conn.cursor()
         cursor.execute('''
             INSERT INTO EXERCISE_TRACKING (starting_timestamp, ending_timestamp, exercise_name, calories_burned, repetitions, steps, comments)
@@ -51,7 +105,7 @@ def log_workout():
 
 @app.route('/workouts')
 def view_workouts():
-    conn = sqlite3.connect("workouts.db")
+    conn = sqlite3.connect("healthfit.db")
     cursor = conn.cursor()
     cursor.execute("SELECT starting_timestamp, ending_timestamp, exercise_name, calories_burned, repetitions, steps, comments FROM EXERCISE_TRACKING ORDER BY starting_timestamp DESC")
     workouts = cursor.fetchall()
@@ -61,7 +115,7 @@ def view_workouts():
 
 @app.route('/calories_chart')
 def calories_chart():
-    conn = sqlite3.connect("workouts.db")
+    conn = sqlite3.connect("healthfit.db")
     cursor = conn.cursor()
     cursor.execute("SELECT starting_timestamp, calories_burned FROM EXERCISE_TRACKING ORDER BY starting_timestamp ASC")
     data = cursor.fetchall()
@@ -135,6 +189,157 @@ def get_sleep_data():
 def sleep_chart():
     return render_template('sleep_chart.html')
 
+
+#Weight logs
+@app.route('/log_weight', methods=['GET', 'POST'])
+def log_weight():
+    if request.method == 'POST':
+        timestamp = request.form.get('timestamp')
+        weight = request.form.get('weight')
+        body_fat_percentage = request.form.get('body_fat_percentage')
+
+        conn = sqlite3.connect("healthfit.db")
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO HISTORICAL_WEIGHT (timestamp, weight, body_fat_percentage)
+            VALUES (?, ?, ?)
+        ''', (timestamp, weight, body_fat_percentage))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('view_weight'))
+    return render_template('log_weight.html')
+
+
+@app.route('/weight_history')
+def view_weight():
+    conn = sqlite3.connect("healthfit.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT timestamp, weight, body_fat_percentage FROM HISTORICAL_WEIGHT ORDER BY timestamp DESC")
+    weight_history = cursor.fetchall()
+    conn.close()
+
+    return render_template('view_weight.html', weight_history=weight_history)
+
+
+@app.route('/weight_chart')
+def weight_chart():
+    conn = sqlite3.connect("healthfit.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT timestamp, weight FROM HISTORICAL_WEIGHT ORDER BY timestamp ASC")
+    data = cursor.fetchall()
+    conn.close()
+
+    weight_data = {
+        "labels": [row[0] for row in data],
+        "values": [row[1] for row in data]
+    }
+    return jsonify(weight_data)
+
+
+@app.route('/view_weight_chart')
+def view_weight_chart():
+    return render_template('weight_chart.html')
+
+
+#Happiness Logs
+@app.route('/log_happiness', methods=['GET', 'POST'])
+def log_happiness():
+    if request.method == 'POST':
+        timestamp = request.form.get('timestamp')
+        happiness = request.form.get('happiness')
+        comments = request.form.get('comments')
+
+        conn = sqlite3.connect("healthfit.db")
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO MOOD_TRACKING (timestamp, happiness, comments)
+            VALUES (?, ?, ?)
+        ''', (timestamp, happiness, comments))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('view_happiness'))
+
+    return render_template('log_happiness.html')
+
+
+
+@app.route('/happiness_index')
+def view_happiness():
+    conn = sqlite3.connect("healthfit.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT timestamp, happiness, comments FROM MOOD_TRACKING ORDER BY timestamp DESC")
+    happiness_index = cursor.fetchall()
+    conn.close()
+
+    return render_template('view_happiness.html', happiness_index=happiness_index)
+
+
+
+@app.route('/happiness_chart')
+def happiness_chart():
+    conn = sqlite3.connect("healthfit.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT timestamp, happiness FROM MOOD_TRACKING ORDER BY timestamp ASC")
+    data = cursor.fetchall()
+    conn.close()
+
+    happiness_data = {
+        "labels": [row[0] for row in data],  
+        "values": [row[1] for row in data]   
+    }
+
+    return jsonify(happiness_data)
+
+@app.route('/view_happiness_chart')
+def view_happiness_chart():
+    return render_template('happiness_chart.html')
+
+
+# Log Nutrition
+@app.route('/log_meal', methods=['GET', 'POST'])
+def log_meal():
+    if request.method == 'POST':
+        
+        meal_category = request.form.get('meal_category')
+        calories = request.form.get('calories')
+        number_of_servings = request.form.get('number_of_servings')
+        weight_grams = request.form.get('weight_grams')
+        vitamins = request.form.get('vitamins')
+        minerals = request.form.get('minerals')
+        carbohydrates = request.form.get('carbohydrates')
+        protein = request.form.get('protein')
+        comments = request.form.get('comments')
+        timestamp = request.form.get('timestamp')
+
+        conn = sqlite3.connect("healthfit.db")
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO MEAL_TRACKING (meal_category, calories, number_of_servings, weight_grams, vitamins, minerals, carbohydrates, protein, comments, timestamp)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (meal_category, calories, number_of_servings, weight_grams, vitamins, minerals, carbohydrates, protein, comments, timestamp))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('view_meals'))
+
+    return render_template('log_meal.html')
+
+
+@app.route('/meals')
+def view_meals():
+    conn = sqlite3.connect("healthfit.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT meal_category, calories, number_of_servings, weight_grams, vitamins, minerals, carbohydrates, protein, comments, timestamp FROM MEAL_TRACKING ORDER BY timestamp DESC")
+    meals = cursor.fetchall()
+    conn.close()
+
+    return render_template('view_meals.html', meals=meals)
+
+
+
+# Home Page
 @app.route('/')
 
 @app.route('/index')
